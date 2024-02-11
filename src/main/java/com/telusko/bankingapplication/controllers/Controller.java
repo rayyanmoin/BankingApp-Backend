d3276b746f354b4f3e59fa3fff493e4fe7a14534
Rayyan Saqib <rayyansaqibmoin@gmail.com>
2024-02-11 15:27:43 +0500

    Add Accounts

--- src/main/java/com/telusko/bankingapplication/controllers/Controller.java
+++ src/main/java/com/telusko/bankingapplication/controllers/Controller.java
@@ -35,30 +35,56 @@ public class Controller {
 
 
     @GetMapping("/users")
-    public List<User> getUser(){
+    public List<User> getUser() {
         List<User> users = userRepository.findAll();
         return users;
     }
 
     @GetMapping("/accounts")
-    public List<Account> getAccount(){
+    public List<Account> getAccount() {
         List<Account> accounts = accountRepository.findAll();
         return accounts;
     }
 
     @GetMapping("/loans")
-    public List<Loan> getLoan(){
+    public List<Loan> getLoan() {
         List<Loan> loans = loanRepository.findAll();
         return loans;
     }
 
     @RequestMapping("/addUser")
-    public User addUser(@RequestParam("name") String name,@RequestParam("status") String status,@RequestParam("CNIC") String CNIC){
+    public String addUser(@RequestParam("name") String name, @RequestParam("status") String status, @RequestParam("CNIC") String CNIC) {
+
+        if (userRepository.existsByCNIC(CNIC)) {
+            return "CNIC already Exits";
+        }
+
         User user = new User();
         user.setName(name);
         user.setStatus(status);
         user.setCNIC(CNIC);
-        return userRepository.save(user);
+        userRepository.save(user);
+        return "User Added Successfully";
+    }
+
+
+    @RequestMapping("/addAccount")
+    public String addAccount(@RequestParam("user_id") long user_id, @RequestParam("balance") Double balance) {
+        if (!userRepository.existsById(user_id)) {
+            return "User with user id Not Found :(";
+        }
+
+        List<Account> accounts = accountRepository.findAll();
+        List<Account> userAccounts = accounts.stream().filter(account1 -> account1.getUserId() == user_id).collect(Collectors.toList());
+        if (userAccounts.size() >= 3) {
+            return "You cannot make more than 3 Accounts in this user Id ";
+        }
+        Account account = new Account();
+        account.setBalance(balance);
+        account.setUserId(user_id);
+        accountRepository.save(account);
+    return "Account Created Successfully";
     }
 
 }
+
--- src/main/java/com/telusko/bankingapplication/repositories/UserRepository.java
+++ src/main/java/com/telusko/bankingapplication/repositories/UserRepository.java
@@ -4,4 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.telusko.bankingapplication.bankingObjects.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByCNIC(String CNIC);
+
+    boolean existsById(long user_id);
+
 }
